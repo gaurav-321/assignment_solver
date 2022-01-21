fb203f9174c003a1ce4fb3504902d747320d78c2
gaurav verma <gauravverma64786@gmail.com>
2022-01-22 02:16:28 +0530

    Final

--- main.py
+++ main.py
@@ -1,62 +1,44 @@
 import os
 import random
+import re
+import shutil
+import traceback
+
 import cv2
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image, ImageChops, ImageMath
 import time, timeit
+from glob import glob
+from random import randint
 
 PAGE_IMG = "pages/notebook_1.jpg"
-FONT_DIR = "fonts/test/my_font/"
-fonts = os.listdir(FONT_DIR)
-# For notebook_1
-SPAN_LENGTH = 2
-ROW_HEIGHT = 5
-ROW_WIDTH = 54
-FONT_SIZE = 25
-ROW_SKIP = 3
-"""
-# For page_3
-SPAN_LENGTH = 2
-ROW_HEIGHT = 5
-ROW_WIDTH = 60
-FONT_SIZE = 20
-ROW_SKIP = 0
+FONTS = glob("fonts\\test\\my_font\\*")
 
-"""
-"""
-# For page_1
-SPAN_LENGTH = 2
-ROW_HEIGHT = 15
-ROW_WIDTH = 49
-FONT_SIZE = 40
-ROW_SKIP = 0
-"""
+
+# Important Functions
+def chunks(l, n):
+    n = max(1, n)
+    return [l[i:i + n] for i in range(0, len(l), n)]
 
 
 def clean_line(input_line):
-    return input_line.strip().replace("#", "")
+    return input_line.strip().replace("#", "").replace("  ", "")
 
 
+# Image load class
 class Page:
     def __init__(self):
         self.lines = []
         self.img_loc = PAGE_IMG
         self.page_img = []
 
-    def load_page(self):
+    def find_lines(self):
         image = cv2.imread(self.img_loc)
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         for y, row in enumerate(gray_img[10:-10]):
             found = np.where(row < 150)[0]
             self.lines.append((found[0], y)) if len(found > 0) else 1
-
-        self.page_img = np.array(image)
-
-    def show_img(self):
-        cv2.imshow("img", self.page_img)
-        cv2.waitKey(0)
-
-    def find_lines(self):
+        self.page_img = image
         for index, row in enumerate(self.page_img):
             if 0 in row:
                 self.lines.append((index, np.where(row == 0)[0][0]))
@@ -68,72 +50,115 @@ class Page:
             self.lines.remove(x)
         self.lines = self.lines[ROW_SKIP:]
 
-
 class Document:
     def __init__(self):
         self.file_loc = "input/document.txt"
-        self.temp_loc = "input/document_edited.txt"
+        self.text_formatted = []
 
-    def document_parse(self, page_obj):
-        text = []
-        total_lines = len(page_obj.lines)
-        with open(self.file_loc) as file:
-            text.extend(file.readlines())
-            finding = True
-            while finding:
-                for index, line in enumerate(text):
-                    if len(clean_line(line)) > ROW_WIDTH:
-                        text[index] = line[:ROW_WIDTH] + "\n"
-                        if line[ROW_WIDTH+1] != "" or line[ROW_WIDTH+2] != "":
-                            if "#" in line:
-                                text.insert(index + 1, "#-" + line[ROW_WIDTH:])
-                            else:
-                                text.insert(index + 1, "-" + line[ROW_WIDTH:])
-                        else:
-                            if "#" in line:
-                                text.insert(index + 1, "#" + line[ROW_WIDTH:])
-                            else:
-                                text.insert(index + 1, "" + line[ROW_WIDTH:])
+    def row_check(self, line):
+        char_limit = ROW_WIDTH
+        count_w = 0
+        for i, char in enumerate(line):
+            if char == " ":
+                count_w += 1
+            if i > char_limit:
+                if " " not in line[i:8]:
+                    count_w += 1
+                return count_w
+        return len(line.split(" "))
+
+    def document_parse(self, total_lines):
+        text_temp = []
+        with open(self.file_loc) as infile:
+            for index, line in enumerate(infile.readlines()):
+                formatting = re.findall("^[#]*", line)  # format stored
+                line = line.replace("#", "").replace("\n", "")
+                line_split = line.split(" ")
+                while True:
+                    ROW_WIDTH = self.row_check(line)  # Number of words in row
+
+                    if len(line_split) > ROW_WIDTH:
+                        line_split = line_split[ROW_WIDTH:]
+                        text_temp += [[formatting[0]] + line_split[:ROW_WIDTH]] if formatting[0] != "" else [
+                            line_split[:ROW_WIDTH]]
+                    else:
+                        text_temp += [[formatting[0]] + line_split[:ROW_WIDTH]] if formatting[0] != "" else [
+                            line_split[:ROW_WIDTH]]
                         break
-                    elif index + 1 >= len(text):
-                        finding = False
-        with open(self.temp_loc, "w") as out:
-            for line in text[:total_lines]:
-                out.write(line)
+        return chunks(text_temp[:-1], total_lines)
 
-    def write_text(self, image, text, location, color, size=FONT_SIZE, gap=0):
-        global SPAN_LENGTH, ROW_HEIGHT
-        font_path = FONT_DIR+random.choice(fonts)
-        font = ImageFont.truetype(font_path, size + random.randint(0, 2))
+    def write_text(self, image, text, location, color, gap=0):
+        font = ImageFont.truetype(FONTS[FONT_LOC], FONT_SIZE + randint(0, 2))
         img_pil = Image.fromarray(image)
         draw = ImageDraw.Draw(img_pil)
-        x, y = location[0] + SPAN_LENGTH + random.randint(-2, 7) + gap, location[1] - ROW_HEIGHT
-        draw.text((x, y), text, font=font, fill=color)
+        x, y = location[0] + randint(-7, 7) + gap + SPAN_LENGTH, location[
+            1] - ROW_HEIGHT
+        draw.text((x, y), clean_line(text), font=font, fill=color)
         return np.array(img_pil)
 
-    def write_from_document(self, page_obj):
-        self.document_parse(page_obj)
-        with open(self.temp_loc, "r") as file:
-            text = file.readlines()
-            for index, line in enumerate(text):
-                gap = 10 + random.randint(1, 10)
-                if "##" in line:
-                    color = (58, 56, 67, 255)
-                    gap = int(page_obj.page_img.shape[1] / 2) - (len(clean_line(line)) / 2) * 14
+    def write_from_list(self, page_obj, list_text):
+        temp_img = page.page_img
+        for index, line in enumerate(list_text):
+            gap = 6 + randint(1, 3)
+            if "##" in line:
+                color = (93, 90, 90, 255)
+                gap = page.page_img.shape[1]/2-len(line)*30
+            elif '#' in line:
+                color = (93, 90, 90, 255)
+            else:
+                color = (131, 80, 66, 255)
+            temp_img = self.write_text(temp_img, " ".join(line),
+                                       page_obj.lines[index], color, gap=gap)
+        return temp_img
 
-                elif '#' in line:
-                    color = (93, 90, 90, 255)
-                else:
-                    color = (131, 80, 66, 255)
-                page.page_img = self.write_text(page.page_img, clean_line(line).replace(" ", "  "),
-                                                page_obj.lines[index], color, gap=gap)
+
+def nothing(*args):
+    pass
+
+
+def init():
+    shutil.rmtree("output")
+    os.mkdir("output")
+    cv2.namedWindow("Threshold", cv2.WINDOW_NORMAL)
+    cv2.createTrackbar("SPAN_LENGTH", "Threshold", SPAN_LENGTH, 100, nothing)
+    cv2.createTrackbar("ROW_HEIGHT", "Threshold", ROW_HEIGHT, 50, nothing)
+    cv2.createTrackbar("ROW_WIDTH", "Threshold", ROW_WIDTH, 100, nothing)
+    cv2.createTrackbar("FONT_SIZE", "Threshold", FONT_SIZE, 80, nothing)
+    cv2.createTrackbar("ROW_SKIP", "Threshold", ROW_SKIP, 20, nothing)
+    cv2.createTrackbar("FONT_LOC", "Threshold", FONT_LOC, len(FONTS) - 1, nothing)
+
+
+def update_vals():
+    global SPAN_LENGTH, ROW_HEIGHT, ROW_WIDTH, FONT_SIZE, ROW_SKIP, FONT_LOC
+    SPAN_LENGTH, ROW_HEIGHT, ROW_WIDTH, FONT_SIZE, ROW_SKIP, FONT_LOC = (
+        cv2.getTrackbarPos('SPAN_LENGTH', 'Threshold'),
+        cv2.getTrackbarPos('ROW_HEIGHT', 'Threshold'),
+        cv2.getTrackbarPos('ROW_WIDTH', 'Threshold'),
+        cv2.getTrackbarPos('FONT_SIZE', 'Threshold'),
+        cv2.getTrackbarPos('ROW_SKIP', 'Threshold'),
+        cv2.getTrackbarPos('FONT_LOC', 'Threshold'))
 
 
 if __name__ == '__main__':
+    with open("last_config.conf") as file:
+        SPAN_LENGTH, ROW_HEIGHT, ROW_WIDTH, FONT_SIZE, ROW_SKIP, FONT_LOC = [int(x) for x in
+                                                                             file.read().split(",")]
+    init()
     page = Page()
-    page.load_page()
     page.find_lines()
     document = Document()
-    document.write_from_document(page)
-    print(page.lines)
-    page.show_img()
+    text_formatted = document.document_parse(len(page.lines))
+    for index, current_para in enumerate(text_formatted):
+        try:
+            while True:
+                update_vals()
+                text_formatted = document.document_parse(len(page.lines))
+                final_img = document.write_from_list(page, text_formatted[index])
+                cv2.imshow("ss", final_img)
+                if cv2.waitKey(1000) & 0XFF == ord("q"):
+                    break
+            cv2.imwrite(f"output/{index}.png", final_img)
+        except:
+            print(traceback.print_exc())
+    with open("last_config.conf", "w") as file:
+        file.write(",".join([str(x) for x in [SPAN_LENGTH, ROW_HEIGHT, ROW_WIDTH, FONT_SIZE, ROW_SKIP, FONT_LOC]]))
